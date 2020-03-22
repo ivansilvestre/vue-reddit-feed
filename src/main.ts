0e67dd64e6128e65fae70eb6de36fc7eacce2eed
ivansilvestre <ivanbsilvestre@gmail.com>
2020-03-22 16:18:54 +0000

    add vuebootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   render: h => h(App)
